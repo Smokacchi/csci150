2c3874f0bd01499400fb9ee374ae4e30394d0883
Smokacchi <113170205+Smokacchi@users.noreply.github.com>
2025-03-19 21:00:31 -0700

    Lab 4 problem 6

---
+++ SebastienVongLab4_6.asm
@@ -0,0 +1,31 @@
+;Write a program with a loop and indexed addressing that exchanges every pair of values in an array
+;with an even number of elements. Therefore, item i will exchange with item i+1, and item i+2 will
+;exchange with item i+3, and so on.
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto,dwExitCode:dword
+
+.data
+	myArray		BYTE	10h, 20h, 30h, 40h, 50h, 60h
+
+
+
+.code
+main proc
+	mov		esi, OFFSET myArray
+	mov		ecx, SIZEOF myArray/2
+
+L1:
+	mov al, [esi]
+	mov bl, [esi+1]
+	inc esi
+	inc esi
+
+	loop L1
+
+
+	invoke ExitProcess,0
+main endp
+end main
